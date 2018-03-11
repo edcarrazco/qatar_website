fd539a482a158d6fd2664dbadb8325dfb21352d0
Edgar Carrasco <edgar.carrasco@wunderman.com>
2018-03-11 09:30:32 -0500

    add data base

--- wp-config.php
+++ wp-config.php
@@ -19,6 +19,8 @@
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
+define  ('WPLANG', 'es_ES');
+
 /** The name of the database for WordPress */
 define('DB_NAME', 'db_qatar');
 
